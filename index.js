f6fe71c0d82c417a62a66d1e99d12f09506c4b73
Jakub Richardson <pythonbottest2020@gmail.com>
2021-09-09 08:38:36 +0200

    moving pages into templates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 
 const SportsDay = require("./models/sportsDay");
@@ -17,18 +18,20 @@ mongoose.connect("mongodb://localhost:27017/sportsApp", {
         console.log("Error:" + err);
     })
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.redirect("/sportsDays");
 })
 
 app.get("/sportsDays", async (req, res) => {
-    const sportsDays = await SportsDay.find({});
+    const sportsDays = await SportsDay.find({}).populate({ path: "events" });
     res.render("sportsDays/index", { sportsDays });
 })
 
